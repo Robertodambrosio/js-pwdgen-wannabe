442d03dfca5064065687860749275de65516ddef
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-04 15:10:13 +0100

    generatore pw

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 // ========= TEST ALTERT JS ==========
 
 // alert ('Hello world');
+
+// ======ins variabili dati 
+
+let myName = prompt('Inserisci il tuo nome');
+document.getElementById('myName').innerHTML = myName;
+console.log(myName);
+
+let lastName = prompt('Inserisci il tuo cognome');
+document.getElementById('lastName').innerHTML = lastName;
+console.log(lastName);
+
+let favCol = prompt('Inserisci il tuo colore preferito');
+document.getElementById('favCol').innerHTML = favCol;
+console.log(favCol);
+
+// ======generatore pw 
+
+const pwGen = myName + lastName + favCol + 21;
+document.getElementById('pwGen').innerHTML = pwGen;
+console.log(pwGen);
